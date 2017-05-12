49dd085b7110c93ddc7e04a192f80136107a0292
John Heerman <jheerman@gmail.com>
2017-05-12 20:08:41 +0000

    Clean API code

--- src/api/outrider.py
+++ src/api/outrider.py
@@ -15,48 +15,30 @@ def index():
 
 @app.route('/college/api/v1.0/colleges', methods=['GET'])
 def get_all_colleges():
-    results = colleges.find().limit(10)
-
-    cleaned_data = json.loads(json_util.dumps(results))    
-    if cleaned_data:
-        return jsonify({"status": "ok", "data": cleaned_data})
-    else:
-	return jsonify({"status": "error", "data":[]})
+    results = colleges.find()
+    return clean_and_jsonify(results);
 
 @app.route('/college/api/v1.0/colleges/<school_name>', methods=['GET'])
 def get_college(school_name): 
     results = colleges.find({"school": school_name})
-
-    cleaned_data = json.loads(json_util.dumps(results))    
-    if cleaned_data:
-        return jsonify({"status": "ok", "data": cleaned_data})
-    else:
-	return jsonify({"status": "error", "data":[]})
+    return clean_and_jsonify(results);
 
 @app.route('/college/api/v1.0/colleges/divisions/<division>', methods=['GET'])
 def get_colleges_by_division(division): 
     results = colleges.find({"division": division})
-
-    cleaned_data = json.loads(json_util.dumps(results))    
-    if cleaned_data:
-        return jsonify({"status": "ok", "data": cleaned_data})
-    else:
-	return jsonify({"status": "error", "data":[]})
+    return clean_and_jsonify(results);
 
 @app.route('/college/api/v1.0/colleges/states/<state_abbr>', methods=['GET'])
 def get_colleges_by_state(state_abbr): 
     results = colleges.find({"state_abbr": state_abbr})
-
-    cleaned_data = json.loads(json_util.dumps(results))    
-    if cleaned_data:
-        return jsonify({"status": "ok", "data": cleaned_data})
-    else:
-	return jsonify({"status": "error", "data":[]})
+    return clean_and_jsonify(results);
 
 @app.route('/college/api/v1.0/colleges/states/<state_abbr>/division/<division>', methods=['GET'])
 def get_colleges_by_state_and_division(state_abbr, division): 
     results = colleges.find({"state_abbr": state_abbr, "division": division})
+    return clean_and_jsonify(results);
 
+def clean_and_jsonify(results):
     cleaned_data = json.loads(json_util.dumps(results))    
     if cleaned_data:
         return jsonify({"status": "ok", "data": cleaned_data})
