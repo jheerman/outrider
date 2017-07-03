b1efbb67235580722fe9ea9164a399b5b5aab8b7
John Heerman <jheerman@gmail.com>
2017-07-03 23:06:33 +0000

    Renamed admissions data and rename api base url

--- src/api/outrider.py
+++ src/api/outrider.py
@@ -7,74 +7,78 @@ import re
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.college
+db = client.outpost
 colleges = db.colleges
 teams = db.softball
 admissions = db.admissions
 coaches = db.coaches
 
-@app.route('/')
-def index():
-    return "Hello, Outrider!"
+@app.route('/info')
+def info():
+    return '{ "name": "recruitingoutpost.com", "api": "v1.0", "author": "odessian"}'
 
-@app.route('/college/api/v1.0/colleges', methods=['GET'])
+@app.route('/outpost/api/v1.0/doc')
+def doc():
+    return '{ "name": "recruitingoutpost.com", "api": "v1.0", "author": "odessian"}'
+
+@app.route('/outpost/api/v1.0/colleges', methods=['GET'])
 def get_all_colleges():
     results = colleges.find()
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/<school_name>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/<school_name>', methods=['GET'])
 def get_college(school_name): 
     results = colleges.find({"school": re.compile(school_name, re.IGNORECASE)}).sort("school")
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/teams/<team_name>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/teams/<team_name>', methods=['GET'])
 def get_college_team(team_name): 
     results = colleges.find({"$or":[{"school": re.compile(team_name, re.IGNORECASE)}, {"team": re.compile(team_name, re.IGNORECASE)}]}).sort("school")
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/divisions/<division>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/divisions/<division>', methods=['GET'])
 def get_colleges_by_division(division): 
     results = colleges.find({"division": division})
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/states/<state_abbr>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/states/<state_abbr>', methods=['GET'])
 def get_colleges_by_state(state_abbr): 
     results = colleges.find({"state_abbr": state_abbr})
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/states/<state_abbr>/division/<division>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/states/<state_abbr>/division/<division>', methods=['GET'])
 def get_colleges_by_state_and_division(state_abbr, division): 
     results = colleges.find({"state_abbr": state_abbr, "division": division})
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/softball', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/softball', methods=['GET'])
 def get_all_softball():
     results = teams.find()
     return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/<school_name>/softball', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/<school_name>/softball', methods=['GET'])
 def get_softball_for_school(school_name):
 	results = teams.find({'school': re.compile(school_name, re.IGNORECASE)})
 	print results;
 	return clean_and_jsonify(results);
 
-@app.route('/college/api/v1.0/colleges/softball/coaches/<coach_name>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/softball/coaches/<coach_name>', methods=['GET'])
 def get_softball_coaches(coach_name):
     results = coaches.find({"$or": [{'name': re.compile(coach_name, re.IGNORECASE)},{'college': re.compile(coach_name, re.IGNORECASE)}]}) 
     return clean_and_jsonify(results);
 
 
-@app.route('/college/api/v1.0/colleges/admissions/<school_name>', methods=['GET'])
+@app.route('/outpost/api/v1.0/colleges/admissions/<school_name>', methods=['GET'])
 def get_college_admissions(school_name):
 	results = admissions.find({'college': re.compile(school_name, re.IGNORECASE)}) 
 	return clean_and_jsonify(results)
 
 def clean_and_jsonify(results):
-    cleaned_data = json.loads(json_util.dumps(results))    
-    if cleaned_data:
-        return jsonify({"status": "ok", "data": cleaned_data})
-    else:
-	return jsonify({"status": "error", "data":[]})
+	cleaned_data = json.loads(json_util.dumps(results))    
+	if cleaned_data:
+		return jsonify({"status": "ok", "data": cleaned_data})
+	else:
+		return jsonify({"status": "error", "data":[]})
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
